7983f32910a16780815b977e26158db0a0da32d4
Armin Burchardt (Elektrobit) <armin.burchardt@elektrobit.com>
2020-12-12 11:01:21 +0100

    ...

--- src/main.rs
+++ src/main.rs
@@ -13,12 +13,12 @@ use hyper::{Body, Request, Response, Server};
 
 #[repr(C)] pub struct EIBConnection { }
 
-#[link(name="eibclient")]
+// #[link(name="eibclient")]
 
 extern {
-    fn EIBSocketURL(url: *const u8) -> *mut libc::c_void;
+    fn _EIBSocketURL(url: *const u8) -> *mut libc::c_void;
     fn _EIBSocketLocal(path: *const u8) -> *mut libc::c_void;
-    fn malloc(len: i64) -> *mut libc::c_void;
+    fn _malloc(len: i64) -> *mut libc::c_void;
     fn _EIBClose(conn: *mut libc::c_void) -> i64;
     fn _EIBComplete(conn: *mut libc::c_void) -> i64;
     fn _EIBSendAPDU(conn: *mut libc::c_void, len: usize , buf: *const u8) -> i64;
@@ -27,8 +27,20 @@ extern {
 #[derive(Debug, Copy, Clone)]
 struct Received { time: std::time::SystemTime, source: EibAddr, dest: EibAddr }
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 struct Measurement { received: Received, value: f32 }
+impl Measurement {
+    pub fn new() -> Self {
+        Self {
+            received: Received {
+                time: SystemTime::now(),
+                source: EibAddr(0, 0, 0),
+                dest: EibAddr(0, 0, 0)
+            },
+            value: 0.0
+        }
+    }
+}
 
 #[derive(Debug)]
 struct DimmerZielwert { received: Received, value: u8 }
@@ -36,6 +48,7 @@ struct DimmerZielwert { received: Received, value: u8 }
 #[derive(Debug)]
 struct Data { received: Received, hex_string: String }
 
+#[derive(Debug)]
 struct Bus { value: f32 } //con: *mut libc::c_void }
 
 #[derive(Debug, Copy, Clone, PartialEq)]
@@ -43,10 +56,22 @@ struct EibAddr(u8, u8, u8);
 
 #[derive(Debug)]
 struct Wetter {
+    till: Measurement,
     a: String,
     b: String,
     c: String,
     lampe: String,
+}
+impl Wetter {
+    pub fn new() -> Self {
+        Self {
+            till: Measurement::new(),
+            a: "".to_string(),
+            b: "".to_string(),
+            c: "".to_string(),
+            lampe: "".to_string()
+        }
+    }
 }
 
 
@@ -66,56 +91,68 @@ async fn hello_world(req: Request<Body>,
     // }
 //    println!("METHOD: {:?}", &req.method());
     let m = req.method().clone();
-    let bytes = body::to_bytes(req.into_body()).await.expect("failed!");
-    let body_str = String::from_utf8(bytes.to_vec()).expect("nody was not valid utf8");
+
     //    println!("BODY: {:?}",body_str);
-    if m == "PUT" {
-	let a = KnxPacket { a: body_str }; //.to_owned() };
-	tx.send(a).expect("tx queue full");
-//	if (body_str == "hallo") {
-//	}
+    if m == hyper::Method::PUT {
+        let bytes = body::to_bytes(req.into_body()).await.expect("failed!");
+        let body_str = String::from_utf8(bytes.to_vec()).expect("nody was not valid utf8");
+        let a = KnxPacket { a: body_str }; //.to_owned() };
+        tx.send(a).expect("tx queue full");
+    } else if m == hyper::Method::GET {
+        let mut handlebars = handlebars::Handlebars::new();
+        for a in &[
+            "index.html",
+            "default-style.css",
+            "img/house.svg",
+            "img/thermometer.svg"
+        ] {
+            let uri = format!("/{}", a);
+            let path = format!("template/{}", a);
+            handlebars.register_template_file(&uri, path).expect(format!("Could not register '{}'", a).as_str());
+        }
+        handlebars.register_template_file("/", "template/index.html").expect("Could not register root uri");
+
+
+        let mut _w = wetter.lock().unwrap();
+        // wetter.lock()a.push('.');
+
+        let _b = Bus { value: 23f32 };
+
+        _w.a.push('.');
+
+        #[derive(serde::Serialize)]
+        struct Info {
+            title: std::string::String,
+            bar: i64,
+            addr: String,
+            temp_a: String,
+            temp_b: String,
+            temp_c: String,
+            lampe: String,
+            till: String,
+        }
+
+        let info = Info {
+            title: "Haus".to_owned(),
+            bar: 1231,
+            addr: format!("{:?}", remote_addr.to_string()),
+            temp_a: _w.a.clone(),
+            temp_b: _w.b.clone(),
+            temp_c: _w.c.clone(),
+            lampe: _w.lampe.clone(),
+            till: ((_w.till.value * 10f32).round() / 10f32).to_string()
+        };
+
+
+        let output = handlebars.render(req.uri().to_string().as_str(), &info);
+        if output.is_err() {
+            eprintln!("GET '{:?}': could not render template", req.uri());
+            return Ok(Response::builder().status(hyper::StatusCode::NOT_FOUND).body("Not found.".into()).unwrap());
+        }
+        return Ok(Response::builder().status(200).body(output.unwrap().into()).unwrap());
     }
-
-    //req.body().on_upgrade().await.unwrap().read_to_string().await.unwrap();
-
-
-
-    let mut handlebars = handlebars::Handlebars::new();
-    if handlebars.register_template_file("/", "template/index.html").is_err() {
-        return Ok(Response::builder().status(300).body("ERROR 2".into()).unwrap());
-    }
-
-    let mut _w = wetter.lock().unwrap();
-    // wetter.lock()a.push('.');
-
-    _w.a.push('.');
-
-    #[derive(serde::Serialize)]
-    struct Info {
-        foo: std::string::String,
-        bar: i64,
-        addr: String,
-        temp_a: String,
-	temp_b: String,
-	temp_c: String,
-	lampe: String,
-    }
-
-    let info = Info {
-        foo: "foo".to_owned(),
-        bar: 1231,
-        addr: format!("{:?}", remote_addr.to_string()),
-        temp_a: _w.a.clone(),
-	temp_b: _w.b.clone(),
-	temp_c: _w.c.clone(),
-	lampe: _w.lampe.clone(),
-    };
-
-    let output = handlebars.render("/", &info);
-
-    Ok(Response::builder().status(200).body(output.unwrap().into()).unwrap())
+    Ok(Response::builder().status(hyper::StatusCode::BAD_REQUEST).body("Bad request.".into()).unwrap())
 }
-
 
 fn _mythread(mut stream: TcpStream, _addr: SocketAddr) {
     eprintln!("New connection: {:?}", stream);
@@ -138,8 +175,11 @@ struct KnxPacket {
     a: String,
 }
 
-use std::sync::mpsc::channel; //function
-fn bus_send_thread(rx: std::sync::mpsc::Receiver<KnxPacket>,
+use std::sync::mpsc::channel;
+use std::time::SystemTime;
+
+//function
+fn _bus_send_thread(rx: std::sync::mpsc::Receiver<KnxPacket>,
 		   u: &std::net::UdpSocket,
 		   _con: Bus) {
     println!("bus.value={:?}", _con.value);
@@ -218,12 +258,12 @@ fn bus_receive_thread(u: &std::net::UdpSocket, data: Arc<Mutex<Wetter>>) {
 	    // Tills Licht AUS:  10 06 00 0f 02 01 29 bc 12 04 04 01 d1 00 80
             let onoff = buf[14] & 0x1 == 1;
             println!("On-Off: {}", onoff);
-	    let mut _v = data.lock().unwrap();
-	    if onoff {
-		_v.lampe = "ON".to_owned();
-	    } else {
-		_v.lampe = "OFF".to_owned();
-	    }
+	        let mut v = data.lock().unwrap();
+	        if onoff {
+		        v.lampe = "ON".to_owned();
+	        } else {
+		        v.lampe = "OFF".to_owned();
+	        }
         }
         if len == 16 {
             // Wert 0..255 unsigned (z.B. Zielwert Dimmer setzen)
@@ -245,18 +285,19 @@ fn bus_receive_thread(u: &std::net::UdpSocket, data: Arc<Mutex<Wetter>>) {
 
 
             let val = Measurement { received: r, value: value };
-            let mut _v = data.lock().unwrap();
+            let mut v = data.lock().unwrap();
 
-	    if val.received.dest == EibAddr(0, 3, 4 ) {
-		// gruppenadresse
-		_v.b = val.value.to_string();
-	    }
-	    if val.received.dest == EibAddr(0, 3, 0 ) {
-		// gruppenadresse
-		_v.c = val.value.to_string();
-	    }
+	        if val.received.dest == EibAddr(0, 3, 4 ) {
+		        // gruppenadresse: Temperatur Till
+		        v.b = val.value.to_string();
+                v.till = val; // copy 'Measurement'
+	        }
+	        if val.received.dest == EibAddr(0, 3, 0 ) {
+		        // gruppenadresse: Temperatur Schrankzimmer
+		        v.c = val.value.to_string();
+	        }
 
-            _v.a = val.value.to_string();
+            v.a = val.value.to_string();
             println!("{:?}\n", val);
             let line = format!("{:?}\n", val);
             logfile.write_all(line.as_bytes()).expect("could not append to buffer");
@@ -269,37 +310,30 @@ fn bus_receive_thread(u: &std::net::UdpSocket, data: Arc<Mutex<Wetter>>) {
 #[tokio::main]
 async fn main() {
 //    let buf = &[10u8, 20, 20, 0];
-    let con: *mut libc::c_void;
-    unsafe {
+//     let con: *mut libc::c_void;
+    // unsafe {
 	//con = malloc(100);
 	// let con = EIBSocketLocal("/run/knx".as_ptr());
 	//
-	let con = EIBSocketURL("ip::3671");
+	// let con = EIBSocketURL("ip::3671".as_ptr());
 //	EIBClose(con);
-	if con == std::ptr::null_mut() { panic!("no connection to knxd"); }
+// 	if con == std::ptr::null_mut() { panic!("no connection to knxd"); }
 
 	//let _l = EIBSendAPDU(con, buf.len(), &buf[0]);
-    }
+    // }
     // let addr = std::net::SocketAddrV4::from_str("0.0.0.0:1234").unwrap();
-    let shared_data = Arc::new(Mutex::new(Wetter {
-        a: "Ah".to_owned(),
-        b: "Beh".to_owned(),
-        c: "Zeh".to_owned(),
-	lampe: "?".to_owned(),
-    }));
-
-
+    let shared_data = Arc::new(Mutex::new(Wetter::new() ));
 
     let u = std::net::UdpSocket::bind("0.0.0.0:51000").expect("Could not bind socket");
     u.join_multicast_v4(
         &std::net::Ipv4Addr::from_str("239.192.39.238").unwrap(),
-        &std::net::Ipv4Addr::from_str("192.168.0.208").unwrap()).expect("join_multicast_v4()");
+        &std::net::Ipv4Addr::from_str("192.168.0.78").unwrap()).expect("join_multicast_v4()");
 
     u.set_multicast_loop_v4(true).expect("set_multicast_loop()");
 
     let bus_data = shared_data.clone();
-    let (tx, rx) = channel();
-    let bus = Bus { value: 123. };
+    let (tx, _rx) = channel();
+    // let bus = Bus { value: 123. };
 
 //    let u1 = std::net::UdpSocket::bind("0.0.0.0:51000").expect("Could not bind socket");
 
